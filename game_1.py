1bbb3e21bef9fca163fea77452da34bb38000672
Silas Henrique <105889529+Kiamisa@users.noreply.github.com>
2025-05-20 11:21:51 -0300

    Update game_1.py

--- game_1.py
+++ game_1.py
@@ -8,3 +8,19 @@ def clear_all():
         _ = system('clear')
 
 clear_all()
+
+def game():
+
+    clear_all()
+    print("Bem vindo ao jogo da Forca\n")
+    print("Adivinhe a palavra abaixo:\n")
+
+    words = ['carro', 'casa', 'helicoptero', 'soldado', 'peste', 'arroba', 'python', 'java', 'windows']
+
+    word = random.choice(words)
+
+    discovered_letter = ['_' for letter in word]
+
+    chances = 6
+
+    wrong_letter = []
